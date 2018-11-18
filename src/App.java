f18e49cb48b9503f4479269788ebc2efd7a5530e
Chromosomus <etacarinae15@mail.ru>
2018-11-18 10:49:46 +0200

    added partitioning and comma sep. string

--- src/App.java
+++ src/App.java
@@ -10,6 +10,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.*;
 
 
+
 public class App {
 
     public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
@@ -27,7 +28,7 @@ public class App {
         ArrayList<User> users = new ArrayList<>();
         users.add(user1);
         User user2 = new User("Pietro", false, new ArrayList<>(List.of("dadads", "dsasdf", "dagdfgfd")), new ArrayList<>(List.of("dsdsfa", "dsadfds")), 212.3, LocalDateTime.of(1598,2,5,2,40,2));
-        User user3 = new User("Bobby Lee", true, new ArrayList<>(List.of("dadafdsfas", "dsdhfgs", "dfsdgdfga")), new ArrayList<>(List.of("dsafsdfdf", "dsfdfdfad")), 103200, LocalDateTime.of(198,12,31,4,20,2));
+        User user3 = new User("Bobby Mcgee", true, new ArrayList<>(List.of("dadafdsfas", "dsdhfgs", "dfsdgdfga")), new ArrayList<>(List.of("dsafsdfdf", "dsfdfdfad")), 103200, LocalDateTime.of(198,12,31,4,20,2));
         users.add(user2);
         users.add(user3);
         User user4 = new User("Boris", false, new ArrayList<>(List.of("dad", "ds", "da")), new ArrayList<>(List.of("dsa", "dsad")), 212.4, LocalDateTime.of(1998,12,1,4,20,2));
@@ -76,7 +77,18 @@ public class App {
         System.out.println(users.stream().sorted(Comparator.comparing(User::getRegistrationDate)).collect(Collectors.toList()));
 
 
-        //
+        //comma separated string
+        System.out.println();
+        System.out.println(users.stream().map(User::getName).collect(Collectors.joining(",")));
+
+        //partitioning
+        System.out.println();
+        Map<Boolean, List<User>> partitionedUsers = users.stream().collect(Collectors.partitioningBy(User::isActive));
+        partitionedUsers.forEach((Boolean k, List<User> u) -> System.out.println(k + " " + u));
+
+        //grouping by roles
+        /*System.out.println();
+        Map<Role,List<User>> userRoles = users.stream().collect(Collectors.groupingBy(User::getRoles));*/
     }
 
 }
